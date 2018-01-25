3f8ece2e5e1fcc9a88a1bf28ef92e26188f9233b
EQUIPO QA TECSIDEL <equipoqa_madrid_valladolid_barcelona@outlook.es>
2018-01-25 15:56:38 +0100

    updated

--- Tajikistan/tajikistanConfiguration/Settingsfields_File.java
+++ Tajikistan/tajikistanConfiguration/Settingsfields_File.java
@@ -15,11 +15,13 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
 public class Settingsfields_File {
-	public static String dateverTransacciones;
+	public static String dateverTransacciones;	
+	public static String hmVersion;
 	public static String BoHostUrl="http://virtualbo-qa/BOQAHostTajikistan/web/forms/core/login.aspx";
 	public static String BoPlazaUrl="http://virtualbo-qa/BOQAPlazaTajikistan/web/forms/core/login.aspx";
 	public static String MCSUrl = "http://virtualmcs-qa/MCS_Tajikistan";
-	public static String RUCid = "ctl00_ContentZone_ctrlAccountData_txt_RUC_box_data";	
+	public static String passPortid = "ctl00_ContentZone_ctrlAccountData_txt_ID_box_data";	
+	public static String taxId = "ctl00_ContentZone_ctrlAccountData_txt_taxNumber_box_data";
 	public static String infoCuenta0 = "ctl00_ContentZone_ctrlAccountData_radio_company_0";
 	public static String infoCuenta1 = "ctl00_ContentZone_ctrlAccountData_radio_company_1";
 	public static String titulofield = "ctl00_ContentZone_ctrlAccountData_cmb_title_cmb_dropdown";
@@ -35,9 +37,7 @@ public class Settingsfields_File {
 	public static String perPhone = "ctl00_ContentZone_ctrlAccountData_txt_homephone_box_data";
 	public static String faxPhone = "ctl00_ContentZone_ctrlAccountData_txt_fax_box_data";
 	public static String companyf = "ctl00_ContentZone_ctrlAccountData_txt_company_box_data";
-	public static String contactf = "ctl00_ContentZone_ctrlAccountData_txt_contact_box_data";
-	public static int carModelSel;
-	public static String mcsVersion = "lbl_version"; 
+	public static String contactf = "ctl00_ContentZone_ctrlAccountData_txt_contact_box_data";	
 	public static String confirmationMessage;
 	public static boolean errorTagAssignment = false;
 	public static String tagIdNmbr;
@@ -59,11 +59,7 @@ public class Settingsfields_File {
 	public static SimpleDateFormat sft;
 	private static int dateMFrom;
 	public static int delm;
-	public static String caMe;
-	public static String acam;	
 	public static String fileDatafilled;
-	public static int ad;
-	public static int caMer;
 	public static String fileError;
 	public static String folderSel;
 	public static String projectSel;
@@ -73,9 +69,8 @@ public class Settingsfields_File {
 	public static WebDriver driver;	
 	public boolean acceptNextAlert = true;
 	public static StringBuffer verificationErrors = new StringBuffer();
-	public static int numbering;
-	public static String linkPartes;
-	public static String Types;
+	public static int numbering;	
+	public static String errorText;
 	public static String tipoSel;
 	public static String loginField = "BoxLogin";
 	public static String passField = "BoxPassword";
@@ -90,16 +85,16 @@ public class Settingsfields_File {
 	public static String hourNumber = "ctl00_ContentZone_TxtNomHousr_box_data";
 	public static String submitBtn = "ctl00_ButtonsZone_BtnSubmit";	
 	public static String MCSVersion;
-	public static String BOVersion;
-	public static String HMVersion;
+	public static String BOVersion;	
 	public static String [] nameOp = new String[] {"Pilar", "Mavi", "Franklyn", "Gemma", "Fatima", "Marc", "Miguel", "Francisco", "Oscar", "Maria"};
 	public static String [] genderS = new String[] {"Female", "Female", "Male", "Female", "Female", "Male", "Male", "Male", "Masculino", "Female"};
 	public static String [] sexSelc = new String[] {"Mrs", "Mrs", "Mr", "Mrs", "Mrs", "Mr", "Mr", "Mr", "Mr", "Mrs"};
 	public static String [] addressTec = new String[] {"CALLE SAN MAXIMO, 3","CALLE SAN MAXIMO, 3","Castanyer 29", "CALLE SAN MAXIMO, 3","CALLE SAN MAXIMO, 3","Catanyer 29","Edificio Tecsidel, P.T. de Boecillo","Edificio Tecsidel, P.T. de Boecillo","Edificio Tecsidel, P.T. de Boecillo","Edificio Tecsidel, P.T. de Boecillo"};
-	public static String [] cpAdress = new String[]{"28041", "28041", "08022", "28041", "28041","08022","47151","47151","47151","47151"};
+	public static String [] cpAdress = new String[]{"280410", "280410", "080220", "280410", "280410","080220","471510","471510","471510","471510"};
 	public static String [] townC = new String []{"Madrid", "Madrid", "Barcelona", "Madrid", "Madrid", "Barcelona", "Valladolid","Valladolid","Valladolid","Valladolid"};
 	public static String [] workPhone1 = new String []{"913530810","913530810","932922110","913530810","913530810","932922110","983546603","983546603","983546603","983546603"};
 	public static String [] lastNameOp = new String[] {"Bonilla", "Garrido", "Garcia", "Arjonilla", "Romano", "Navarro", "Sanchez", "Castro", "Bailon", "Blanco"};
+	public static String [] lastNameOp2 = new String[] {"Rol", "Sanchez", "Noyola", "Yague", "Cerror", "Ordoñez", "Grande", "Castaño", "Azcaray", "Ramos"};
 	  //Edit buttons icons configuration.	  	  
 	  public static Timestamp timest = new Timestamp (System.currentTimeMillis());
 	  public static String timet = timest.toString().replace("-", "").replace(" ", "").replace(":", "").substring(0,14);
@@ -108,6 +103,7 @@ public class Settingsfields_File {
 		    File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
 		    FileUtils.copyFile(scrFile, new File(pathS, fname));
 	  }
+	  
 	  public static void borrarArchivosTemp(String tempPath) throws Exception{
 			File imagTmp = new File (tempPath);
 			if (imagTmp.exists()){
@@ -124,128 +120,146 @@ public class Settingsfields_File {
 			Thread.sleep(1000);
 		  }	
 	  
-	  public static void selectDropDownClick2(String by)
-      {
-          Select vDropdown = new Select (driver.findElement(By.id(by)));
-          List<WebElement> dd = vDropdown.getOptions();
-          Random rand = new Random();
-          int vdd = rand.nextInt(dd.size())+1;
-          if (vdd <= 0) { vdd = vdd + 1; }
-          	
-          if (vdd >= dd.size()) { vdd = dd.size() - 1; }
-          new Select(driver.findElement(By.id(by))).selectByIndex(vdd);
-
-      }
 	  
-	  public static void ranSelection(String selId, int len1) throws Exception{
-			List <WebElement >mcCameras = driver.findElements(By.xpath("//*[contains(@id, '"+selId+"')]"));        
-	        caMe = mcCameras.get(0).getAttribute("id");
-	        acam = mcCameras.get(mcCameras.size()-1).getAttribute("id");
-	        ad = Integer.parseInt(caMe.substring(len1));
-	        caMer = Integer.parseInt(acam.substring(len1));
-		}
-	  
-	  public static void selectDropDownClick(String by)
-      {
-          Select vDropdown = new Select (driver.findElement(By.id(by)));
-          List<WebElement> dd = vDropdown.getOptions();
-          Random rand = new Random();
-          int vdd = rand.nextInt(dd.size())+1;
-          if (vdd < 0) { vdd = 0; }
-          	
-          if (vdd >= dd.size()) { vdd = dd.size() - 1; }
-          new Select(driver.findElement(By.id(by))).selectByIndex(vdd);
 
-      }
-	  
-	  public static void clickAll(String id, int camp1, int camp2) throws Exception{          
-              for (int i = camp1; i <= camp2; i=i +2){
-              Thread.sleep(200);
-              driver.findElement(By.id(id + i)).click();
-              }
-	  	}
+    	public static void elementClick(String byID) {
+    		if (byID.contains("//")){
+    			driver.findElement(By.xpath(byID)).click();
+    		}else{
+    			driver.findElement(By.id(byID)).click();
+    		}
+    		
+    	}
+    	
+    	
+    	
+   	public static void selectDropDownV(String by) {     		
+    		Select vDropdown = new Select (driver.findElement(By.id(by)));
+    			List<WebElement> dd = vDropdown.getOptions();		
+    			Random rand = new Random();
+    		int vdd = rand.nextInt(dd.size());
+    			if (vdd<=0){vdd = 1;}	
+    			if (vdd>=dd.size()){vdd=vdd-1;}      			      			
+    		new Select (driver.findElement(By.id(by))).selectByIndex(vdd);
+    		
+    		
+    	  }
+    	
+    	public static void selectDropDown(String by) {
+    		Select vDropdown = new Select (driver.findElement(By.id(by)));
+    			List<WebElement> dd = vDropdown.getOptions();		
+    			Random rand = new Random();
+    		int vdd = rand.nextInt(dd.size());
+    			if (vdd<0){vdd = 0;}	
+    			if (vdd>=dd.size()){vdd=vdd-1;}
+    		new Select (driver.findElement(By.id(by))).selectByIndex(vdd);
+    		
+    	  }
+    	public static void selectDropDownXPath(String xpath) {
+    		Select vDropdown = new Select (driver.findElement(By.xpath(xpath)));
+    			List<WebElement> dd = vDropdown.getOptions();		
+    			Random rand = new Random();
+    		int vdd = rand.nextInt(dd.size());
+    			if (vdd<0){vdd = 0;}	
+    			if (vdd>=dd.size()){vdd=vdd-1;}
+    		new Select (driver.findElement(By.xpath(xpath))).selectByIndex(vdd);
+    		
+    	  }
 
-      	public static void elementClick(String byID) {
-      			driver.findElement(By.id(byID)).click();
-      		}
-      	
-      	public static void selectDropDown(String by) {
-      		Select vDropdown = new Select (driver.findElement(By.id(by)));
-      			List<WebElement> dd = vDropdown.getOptions();		
-      			Random rand = new Random();
-      		int vdd = rand.nextInt(dd.size());
-      			if (vdd<0){vdd = 0;}	
-      			if (vdd>=dd.size()){vdd=vdd-1;}
-      		new Select (driver.findElement(By.id(by))).selectByIndex(vdd);
-      		
-      	  }
-      	
-     	public static void ranclickOption(String[] id, int min, int max) throws Exception{
- 			
-     		Random rand = new Random();
-     		int selOp = rand.nextInt(max-min)+1;
-     		if (selOp >= id.length){
-     			selOp = selOp -1;
-     		} 		
-     		for (int i = 1; i <= selOp;i++ ){
-     			
-     			if (selOp == id.length -1){
-     				if (!driver.findElement(By.id(id[i])).isSelected()){
-     					driver.findElement(By.id(id[i])).click();
-     					Thread.sleep(300);
-     				}	
-     			}else{
-     			int selc = rand.nextInt(max-min)+1;
-     			if (selc == id.length){
-     				selc = selc - 1;
-     			}
-     			if (!driver.findElement(By.id(id[i])).isSelected()){
-     					driver.findElement(By.id(id[selc])).click();
-     						Thread.sleep(1000);
-     					}	
-     	 			}	
-     		}	
-     		
-     	}
-     	
-     	public static void dateSel() throws Exception{
+   	
+   	public static String dateSel(int date1,int date2 ) throws Exception{
 			calF = Calendar.getInstance();
 			calT = Calendar.getInstance();
 		 sft = new SimpleDateFormat("dd/MM/yyyy");
-		calF.set(ranNumbr(2017,2018), dateMFrom, ranNumbr(1,31));
-		calT.set(ranNumbr(1972,1979), ranNumbr(1,12), ranNumbr(1,31));
+		calF.set(ranNumbr(date1,date2), dateMFrom, ranNumbr(1,31));
+		calT.set(ranNumbr(date1,date2), ranNumbr(1,12), ranNumbr(1,31));
+		return sft.format(calT.getTime());
 
 	}
-     	public static int ranNumbr(int min, int max) {
-   		  Random rand = new Random();
-   		  numbering = min+rand.nextInt((max-min)+1);
-   		  return numbering;
-   		  
-   	  }
-     	public static void notEmptyDropDown(String by) throws Exception{
-            Select fDropDown = new Select (driver.findElement(By.id(by)));
-            List<WebElement> fDsel = fDropDown.getOptions();
-            fDsel.size();             
-            if (fDsel.size() > 1){
-            	selectDropDownClick(by);
-            }
-            Thread.sleep(1000);
-     	}
+   	public static int ranNumbr(int min, int max) {
+ 		  Random rand = new Random();
+ 		  numbering = min+rand.nextInt((max-min)+1);
+ 		  if (numbering <0){
+ 			 numbering = min+rand.nextInt((max-min)+1);
+ 		  }
+ 		  return numbering; 		  
+ 	  }
 
-     	public static int ranYearNumbr(int min, int max) {
-     		  Random rand = new Random();
-     		  numbering = min+rand.nextInt((max-min)+1);
-     		  return numbering;    		  
-       	}
-     	
-     	public static void loginPage(String usr, String pwd) throws Exception{
-     			driver.findElement(By.id(loginField)).sendKeys(usr);
-     			driver.findElement(By.id(passField)).sendKeys(pwd);
-     			driver.findElement(By.id(loginButton)).click();
-     			Thread.sleep(1000);
-     	}
-     	/*public static String dniLetra (int dni){
-  		  return String.valueOf(dni)+(NIF_STRING_ASOCIATION.charAt(dni % 23));
-  	  }*/
-      	
+   	public static void loginPageMCS(String usr, String pwd) throws Exception{
+			driver.findElement(By.id("txt_login")).sendKeys(usr);
+			driver.findElement(By.id("txt_password")).sendKeys(pwd);
+			driver.findElement(By.id("btn_login")).click();
+			Thread.sleep(1000);
+	}
+   	
+   	public static void loginPage(String usr, String pwd) throws Exception{
+   			driver.findElement(By.id(loginField)).sendKeys(usr);
+   			driver.findElement(By.id(passField)).sendKeys(pwd);
+   			driver.findElement(By.id(loginButton)).click();
+   			Thread.sleep(1000);
+   	}
+   	
+  /* 	public static String dniLetra (String Dtype, int dni){
+   		if (Dtype == "DNI"){
+   			return String.valueOf(dni)+(matletT.charAt(dni % 23));
+   		}else{
+   			return String.valueOf(matletT.charAt(dni % 23))+(dni);
+   		}
+	  }*/
+   	
+   	public static void SendKeys (String path, String toSend){
+   		
+   		if (path.contains("//")){
+   			driver.findElement(By.xpath(path)).clear();
+   			driver.findElement(By.xpath(path)).sendKeys(toSend);
+   		}else{
+   			driver.findElement(By.id(path)).clear();
+   			driver.findElement(By.id(path)).sendKeys(toSend);    			
+   		}
+   	}
+   	
+   	public static String hourFormat(int Hour1,int Hour2,int Min1, int Min2){
+			int Hour = ranNumbr(Hour1,Hour2);
+			int Min = ranNumbr(Min1,Min2);			
+			return String.format("%1$02d",Hour)+":"+String.format("%1$02d",Min);					
+		}
+		public static boolean isAlertPresent() throws Exception{
+			try{
+				driver.switchTo().alert();
+				return true;
+			}catch (Exception e){
+				return false;
+			}
+
+		}
+	public static String getVersion (String versionSelected){
+   		
+			hmVersion = BOVersion.substring(1);
+			if (BOVersion.contains("_")){								
+				if (BOVersion.length()<17){
+					BOVersion = BOVersion.substring(1);
+					hmVersion = "HM is not Running";
+				}else{
+					hmVersion = BOVersion.substring(18);
+					BOVersion = BOVersion.substring(0, 16);
+					
+				}
+			}else{
+				if (BOVersion.length()<10){
+					BOVersion = BOVersion.substring(1);
+					hmVersion = "HM is not Running";
+				}else{
+					hmVersion = BOVersion.substring(11);
+					BOVersion = BOVersion.substring(0, 9);					
+				}
+			}
+			if (versionSelected == "BO"){
+				versionSelected = BOVersion;
+			}
+			if (versionSelected == "HM"){
+				versionSelected = hmVersion;
+			} 
+			return versionSelected;			
+   	}
+	
 }
